fa1a7563fd9fa2c344d6df4ad5fa9ce40ff93ebf
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-13 19:28:25 +0200

    Telephone mappings

--- BManager/Mappings/MappingConfig.cs
+++ BManager/Mappings/MappingConfig.cs
@@ -1,4 +1,5 @@
 ﻿using BManager.Dtos.Person;
+using BManager.Dtos.Telephone;
 using BManager.Models;
 
 namespace BManager.Mappings
@@ -7,9 +8,13 @@ namespace BManager.Mappings
     {
         public MappingProfile()
         {
-            CreateMap<PersonForCreationDto, Person>();
             CreateMap<Person, PersonGetDto>();
             CreateMap<PersonUpdateDto, Person>();
+            CreateMap<PersonForCreationDto, Person>();
+
+            CreateMap<Telephone, TelephoneGetDto>();
+            CreateMap<TelephoneCreateDto, Telephone>();
+            CreateMap<TelephoneUpdateDto, Telephone>();
         }
     }
 }
