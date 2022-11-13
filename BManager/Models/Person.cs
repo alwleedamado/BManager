c0d2fb1e603df784129a8ac80576ce8937328869
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-13 19:35:32 +0200

    Person required id and name

--- BManager/Models/Person.cs
+++ BManager/Models/Person.cs
@@ -1,11 +1,14 @@
 ﻿using BManager.Utils.Abstractions;
 using BManager.Utils.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace BManager.Models
 {
     public class Person : AuditEntity
     {
+        [Required, MaxLength(100)]
         public string Name { get; set; }
+        [MaxLength(100)]
         public string Email { get; set; }
         public Gender Gender { get; set; }
         public List<Telephone> Telephones { get; set; } = new List<Telephone>();
