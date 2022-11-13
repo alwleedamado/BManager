dc69782a67ef72efbcd4fb37be6e0fcb2fef123e
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-13 19:15:28 +0200

    Polarize routes

--- BManager/Controllers/PersonController.cs
+++ BManager/Controllers/PersonController.cs
@@ -1,25 +1,14 @@
-﻿using AutoMapper;
-using BManager.Data.IRepositories;
-using BManager.Data.Repositories;
-using BManager.Dtos.Person;
-using BManager.Models;
-using BManager.Utils;
-using Microsoft.AspNetCore.Mvc;
-
+﻿
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BManager.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Persons")]
     [ApiController]
-    public class PersonController : TypedController<Person, PersonForCreationDto, PersonGetDto, PersonUpdateDto>
+    public class PersonController : TypedController<Person, PersonForCreationDto, PersonGetDto, PersonUpdateDto, PersonFilter>
     {
         public PersonController(IPersonRepository personRepository, IMapper mapper) : base(personRepository, mapper)
         {
-        }
-
-        // GET: api/<ValuesController>
-        
-      
+        }      
     }
 }
