0c053dd039d3c21cd91719ffdf1ae7a02d733c0f
Alwaleed Ahmed <alwleedamado@gmail.com>
2022-11-13 19:29:55 +0200

    Formating AuditEntity

--- BManager/Utils/Abstractions/AuditEntity.cs
+++ BManager/Utils/Abstractions/AuditEntity.cs
@@ -2,29 +2,11 @@
 {
     public class AuditEntity : Entity
     {
-        public DateTimeOffset CreatedOn
-        {
-            get; set;
-        }
-        public DateTimeOffset UpdatedOn
-        {
-            get; set;
-        }
-        public int CreatedBy
-        {
-            get; set;
-        }
-        public int UpdatedBy
-        {
-            get; set;
-        }
-        public DateTimeOffset? DeletedOn
-        {
-            get; set;
-        }
-        public int DeletedBy
-        {
-            get; set;
-        }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset? UpdatedOn { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTimeOffset? DeletedOn { get; set; }
+        public int DeletedBy { get; set; }
     }
 }
